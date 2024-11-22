4112f2c48cb2a2dad11b9b587b58ab4eafd0b946
derajohnson <derajohnson64@gmail.com>
2024-11-22 14:39:42 +0100

    added suffix and extionsion rename functionality

--- batch-renamer-core/batch_renamer.go
+++ batch-renamer-core/batch_renamer.go
@@ -3,15 +3,17 @@ package batchrenamer
 import (
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"regexp"
+	"strings"
 )
 
-func FileRenamer(directoryPath string, newName string) error {
+func readDirectory(directoryPath string) ([]fs.DirEntry, error) {
 	// opening the directory
 	directory, err := os.Open(directoryPath)
 	if err != nil {
-		return fmt.Errorf("invalid path to directory: %w", err)
+		return nil, fmt.Errorf("invalid path to directory: %w", err)
 	}
 
 	defer directory.Close()
@@ -19,9 +21,15 @@ func FileRenamer(directoryPath string, newName string) error {
 	// reading the files in the directory
 	files, err := directory.ReadDir(-1)
 	if err != nil {
-		return fmt.Errorf("unable to read directory contents: %w", err)
+		return nil, fmt.Errorf("unable to read directory contents: %w", err)
 
 	}
+	return files, nil
+
+}
+
+func AddPrefix(directoryPath string, newName string) error {
+	files, _ := readDirectory(directoryPath)
 
 	for _, file := range files {
 		for i := 0; i <= len(files); i++ {
@@ -37,7 +45,56 @@ func FileRenamer(directoryPath string, newName string) error {
 		}
 
 	}
-	fmt.Println("All files renamed successfully")
+	fmt.Printf("Prefix %v added to all files\n", newName)
+	return nil
+
+}
+
+func AddSuffix(directoryPath string, newName string) error {
+	files, _ := readDirectory(directoryPath)
+
+	for _, file := range files {
+		for i := 0; i <= len(files); i++ {
+			acceptedCharacters := regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9_-]*$`)
+			matchExtension := regexp.MustCompile(`^[^.]*`)
+			extension := matchExtension.ReplaceAllString(file.Name(), "")
+			subString := strings.LastIndex(file.Name(), ".")
+			fileName := file.Name()[:subString]
+			filePath := fmt.Sprintf("%s/%s", directoryPath, file.Name())
+			if acceptedCharacters.MatchString(newName) && !file.IsDir() {
+				newFilePath := fmt.Sprintf("%s/%s_%s%s", directoryPath, fileName, newName, extension)
+				os.Rename(filePath, newFilePath)
+			} else if !acceptedCharacters.MatchString(newName) {
+				return errors.New("new file name must not include special characters")
+			}
+
+		}
+
+	}
+	fmt.Printf("Suffix %v added to all files\n", newName)
+	return nil
+
+}
+
+func ChangeExtension(directoryPath string, extension string) error {
+	files, _ := readDirectory(directoryPath)
+	for _, file := range files {
+		for i := 0; i <= len(files); i++ {
+			acceptedCharacters := regexp.MustCompile(`^\.[a-zA-Z]+$`)
+			subString := strings.LastIndex(file.Name(), ".")
+			fileName := file.Name()[:subString]
+			filePath := fmt.Sprintf("%s/%s", directoryPath, file.Name())
+			if acceptedCharacters.MatchString(extension) && !file.IsDir() {
+				newFilePath := fmt.Sprintf("%s/%s%s", directoryPath, fileName, extension)
+				os.Rename(filePath, newFilePath)
+			} else if !acceptedCharacters.MatchString(extension) {
+				return errors.New("extension must not include special characters, numbers and must start with a dot(.)")
+			}
+
+		}
+
+	}
+	fmt.Printf("Extension %v added to all files\n", extension)
 	return nil
 
 }
--- example-project/main.go
+++ example-project/main.go
@@ -7,10 +7,24 @@ import (
 )
 
 func main() {
-	err := batchrenamer.FileRenamer("./my-files", "velvety")
-	if err != nil {
-		fmt.Println("Error:", err)
+	prefixErr := batchrenamer.AddPrefix("./my-files", "open")
+	if prefixErr != nil {
+		fmt.Println("Error:", prefixErr)
 	} else {
-		fmt.Println("Files renamed successfully!")
+		fmt.Println("Prefix added successfully!")
+	}
+
+	suffixErr := batchrenamer.AddSuffix("./my-files", "source")
+	if suffixErr != nil {
+		fmt.Println("Error:", suffixErr)
+	} else {
+		fmt.Println("Suffix added successfully!")
+	}
+
+	extensionErr := batchrenamer.ChangeExtension("./my-files", ".md")
+	if extensionErr != nil {
+		fmt.Println("Error:", suffixErr)
+	} else {
+		fmt.Println("Extension added successfully!")
 	}
 }
